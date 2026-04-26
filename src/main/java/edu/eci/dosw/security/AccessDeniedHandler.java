ff72c6d268ca3fd08efbb8af12b077a8812b9617
Juan <juan.roa-h@mail.escuelaing.edu.co>
2026-04-26 17:10:28 -0500

    feat: Acces Denied Handler implementation

--- src/main/java/edu/eci/dosw/security/AccessDeniedHandler.java
+++ src/main/java/edu/eci/dosw/security/AccessDeniedHandler.java
@@ -15,17 +15,14 @@ import java.util.Map;
 public class AccessDeniedHandler implements org.springframework.security.web.access.AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-
         Map<String, Object> body = new HashMap<>();
         body.put("timestamp", LocalDateTime.now().toString());
-        body.put("status", 401);
-        body.put("error", "Unauthorized");
-        body.put("message", "Invalid, expired or non-existing Token");
+        body.put("status", 403);
+        body.put("error", "Forbidden");
+        body.put("message", "No tienes permisos para acceder a este recurso");
         body.put("path", request.getRequestURI());
-
         ObjectMapper mapper = new ObjectMapper();
         mapper.writeValue(response.getOutputStream(), body);
     }
