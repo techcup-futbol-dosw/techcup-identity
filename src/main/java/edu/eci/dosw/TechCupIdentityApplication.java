89a59f9e683bb521d59b3cda690c6b4a55285992
Juan <juan.roa-h@mail.escuelaing.edu.co>
2026-04-26 15:03:25 -0500

    feat: application main class

---
+++ src/main/java/edu/eci/dosw/TechCupIdentityApplication.java
@@ -0,0 +1,11 @@
+package edu.eci.dosw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechCupIdentityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TechCupIdentityApplication.class,args);
+    }
+}
