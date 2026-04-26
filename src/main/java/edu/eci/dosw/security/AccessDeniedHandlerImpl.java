169190d937f3ff77b9022b9fe7591684d4b1fb75
Juan <juan.roa-h@mail.escuelaing.edu.co>
2026-04-26 17:20:26 -0500

    feat: AccesDeniedHandlerImpl refactor

--- src/main/java/edu/eci/dosw/security/AccessDeniedHandlerImpl.java
+++ src/main/java/edu/eci/dosw/security/AccessDeniedHandlerImpl.java
@@ -6,13 +6,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
-
-public class AccessDeniedHandler implements org.springframework.security.web.access.AccessDeniedHandler {
+@Component
+public class AccessDeniedHandlerImpl implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
