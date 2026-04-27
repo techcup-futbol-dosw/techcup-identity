3bd4829dda9ff56ac10fb5a73ffb27d4ca50e15c
Juan <juan.roa-h@mail.escuelaing.edu.co>
2026-04-26 19:14:07 -0500

    feat: JwtService esqueleton

--- src/main/java/edu/eci/dosw/service/JwtService.java
+++ src/main/java/edu/eci/dosw/service/JwtService.java
@@ -2,6 +2,30 @@ package edu.eci.dosw.service;
 
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class JwtService {
+
+
+
+
+    public String extractUserId(String token){
+        return "";
+    }
+    public String extractEmail(String token){
+        return "";
+    }
+    public List<String> extractRoles(String token){
+        return new ArrayList<>();
+    }
+
+    public List<String> extractPermissions(String token){
+        return new ArrayList<>();
+    }
+    public boolean isTokenValid(String token){
+        return true;
+    }
 }
